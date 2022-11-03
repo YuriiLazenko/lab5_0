ee32d0b3c61ba72870fb1071bfe1ef7ac7b1b617
Yurii <steamyurilazenko@gmail.com>
2022-11-03 23:51:56 +0200

    lab5.2

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab5.2/lab5.2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int t;
+			t = S(1, 1, 1, 1);
+			Assert::AreEqual(2, t, 0.00001);
+		}
+	};
+}
